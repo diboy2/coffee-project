8f243ee1add9bea8a596f02fce00dbf7e0fc6f17
ac044401 <AC044401@cerner.net>
2018-06-01 13:04:31 -0500

    Separated views

---
+++ src/components/AppContent.vue
@@ -0,0 +1,128 @@
+<template>
+  <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-one-quarter">
+                    <div class="card">
+                        <header class="card-header">
+                            <p class="card-header-title">
+                                Test Test
+                            </p>
+                        </header> 
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/bulma-logo.png" alt="Placeholder image" />
+
+                            </figure>
+                        </div>
+                        <footer class="card-footer">
+                            <a href="#" class="card-footer-item">Save</a>
+                            <a href="#" class="card-footer-item">Edit</a>
+                            <a href="#" class="card-footer-item">Delete</a>
+                        </footer>
+                    </div>
+                </div>
+                <div class="column is-three-quarter">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="content">
+                                <p>
+                                    Contains macaroni, bacon, and cheese. asdohgo piahposierhpoi
+                                </p>
+                                <div class="columns">
+                                    <div class="column is-one-half">
+                                        <div class="columns">
+                                            <div class="column is-one-fifth">
+                                                test
+                                            </div>
+                                            <div class="column is-fourth-fifths">
+                                                <progress class="progress" value="15" max="100">15%</progress>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="column is-one-half">
+                                        <div class="columns">
+                                            <div class="column is-one-fifth">
+                                                test
+                                            </div>
+                                            <div class="column is-fourth-fifths">
+                                                <progress class="progress" value="15" max="100">15%</progress>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="columns">
+                                    <div class="column is-one-half">
+                                        <div class="columns">
+                                            <div class="column is-one-fifth">
+                                                test
+                                            </div>
+                                            <div class="column is-fourth-fifths">
+                                                <progress class="progress" value="15" max="100">15%</progress>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="column is-one-half">
+                                        <div class="columns">
+                                            <div class="column is-one-fifth">
+                                                test
+                                            </div>
+                                            <div class="column is-fourth-fifths">
+                                                <progress class="progress" value="15" max="100">15%</progress>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="columns">
+                                    <div class="column is-one-half">
+                                        <div class="columns">
+                                            <div class="column is-one-fifth">
+                                                test
+                                            </div>
+                                            <div class="column is-fourth-fifths">
+                                                <progress class="progress" value="15" max="100">15%</progress>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="column is-one-half">
+                                        <div class="columns">
+                                            <div class="column is-one-fifth">
+                                                test
+                                            </div>
+                                            <div class="column is-fourth-fifths">
+                                                <progress class="progress" value="15" max="100">15%</progress>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="columns">
+                                    <div class="column is-one-half">
+                                        <div class="columns">
+                                            <div class="column is-one-fifth">
+                                                test
+                                            </div>
+                                            <div class="column is-fourth-fifths">
+                                                <progress class="progress" value="15" max="100">15%</progress>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="column is-one-half">
+                                        <div class="columns">
+                                            <div class="column is-one-fifth">
+                                                test
+                                            </div>
+                                            <div class="column is-fourth-fifths">
+                                                <progress class="progress" value="15" max="100">15%</progress>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+      <div class="container">
+          <div class="navbar-brand">
+              <a class="navbar-item" href="https://bulma.io">
+                  <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+              </a>
+
+              <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+              </a>
+          </div>
+          <div class="navbar-item">
+              <div class="field">
+                  <p class="control has-icons-right">
+                      <input class="input" placeholder="">
+                      <span class="icon is-small is-right">
+                          <i class="fas fa-search"></i>
+                      </span>
+                  </p>
+              </div>
+          </div>
+      </div>
+  </nav>
+</template>
